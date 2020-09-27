0cbee9afd6b778ff8f2f7594af741212d3b5d88a
Diksha Kewat <diksha.kewat>
2020-09-27 19:48:31 +0700

    wsgi error resolution

--- dataMatics/dataMatics/wsgi.py
+++ dataMatics/dataMatics/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dataMatics.settings')
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
